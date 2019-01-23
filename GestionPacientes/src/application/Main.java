45d319c3a88834742463e66b48ce66f05e150415
Pedro Díez <pedromdiez@gmail.com>
2019-01-23 01:03:35 +0100

    runnable

--- GestionPacientes/src/application/Main.java
+++ GestionPacientes/src/application/Main.java
@@ -43,6 +43,7 @@ public class Main extends Application {
 			Files.copy(getClass().getResourceAsStream("/assets/sensors.xml"), Paths.get(cwd+"/sensors.xml"), StandardCopyOption.REPLACE_EXISTING);
 			Files.copy(getClass().getResourceAsStream("/assets/roles.xml"), Paths.get(cwd+"/roles.xml"), StandardCopyOption.REPLACE_EXISTING);
 			Files.copy(getClass().getResourceAsStream("/assets/sensorinput.csv"), Paths.get(cwd+"/sensorinput.csv"), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(getClass().getResourceAsStream("/assets/a.bat"), Paths.get(cwd+"/a.bat"), StandardCopyOption.REPLACE_EXISTING);
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
